9529ea90a76887dfc2ff8f1ccac9f2e0b11bb6d6
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-02-01 19:46:24 +0100

    Validacion Login/Registro FrontEnd

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -7,81 +7,67 @@
             <v-window-item :value="1">
               <v-row>
                 <v-col cols="12" md="6">
-                  <v-card-text class="mt-12">
-                    <h4 class="text-center">Login in to Your Account</h4>
-                    <h6 class="text-center grey--text">
-                      Log in to your account so you can continue builiding
-                      <br />and editing your onboarding flows
-                    </h6>
-                    <v-row align="center" justify="center">
-                      <v-col cols="12" sm="8">
-                        <v-text-field
-                          label="Email"
-                          outlined
-                          dense
-                          color="blue"
-                          autocomplete="false"
-                          class="mt-16"
-                        />
-                        <v-text-field
-                          label="Password"
-                          outlined
-                          dense
-                          color="blue"
-                          autocomplete="false"
-                          type="password"
-                        />
-                        <v-row>
-                          <v-col cols="12" sm="7">
-                            <v-checkbox
-                              label="Remember Me"
-                              class="mt-n1"
-                              color="blue"
-                            >
-                            </v-checkbox>
-                          </v-col>
-                          <v-col cols="12" sm="5">
-                            <span class="caption blue--text"
-                              >Forgot password</span
-                            >
-                          </v-col>
-                        </v-row>
-                        <v-btn color="blue" dark block tile>Log in</v-btn>
+                  <v-form ref="form" v-model="valid" miValidacion>
+                    <v-card-text class="mt-12">
+                      <h4 class="text-center">Inicia Sesión en tu cuenta</h4>
 
-                        <h5 class="text-center grey--text mt-4 mb-3">
-                          Or Log in using
-                        </h5>
-                        <div
-                          class="d-flex justify-space-between align-center mx-10 mb-16"
-                        >
-                          <v-btn depressed outlined color="grey">
-                            <v-icon color="red">fab fa-google</v-icon>
-                          </v-btn>
-                          <v-btn depressed outlined color="grey">
-                            <v-icon color="blue">fab fa-facebook-f</v-icon>
-                          </v-btn>
-                          <v-btn depressed outlined color="grey">
-                            <v-icon color="light-blue lighten-3"
-                              >fab fa-twitter</v-icon
-                            >
-                          </v-btn>
-                        </div>
-                      </v-col>
-                    </v-row>
-                  </v-card-text>
+                      <v-row align="center" justify="center">
+                        <v-col cols="12" sm="8">
+                          <v-text-field
+                            v-model="emailLogIn"
+                            label="Correo Electrónico"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                            class="mt-16"
+                            :rules="validacionMail"
+                            required
+                          />
+                          <v-text-field
+                            v-model="pwdLogIn"
+                            label="Contraseña"
+                            outlined
+                            dense
+                            :rules="validacionContraseña"
+                            color="blue"
+                            autocomplete="false"
+                            type="password"
+                            required
+                          />
+                          <v-row>
+                            <v-col cols="12" sm="7">
+                              <v-checkbox
+                                label="Recordar"
+                                class="mt-n1"
+                                color="blue"
+                              >
+                              </v-checkbox>
+                            </v-col>
+                          </v-row>
+                          <v-btn
+                            :disabled="!valid"
+                            color="success"
+                            dark
+                            block
+                            tile
+                            @click="logIn()"
+                            >INICIAR SESIÓN</v-btn
+                          >
+                        </v-col>
+                      </v-row>
+                    </v-card-text>
+                  </v-form>
                 </v-col>
                 <v-col cols="12" md="6" class="blue rounded-bl-xl">
                   <div style="text-align: center; padding: 180px 0">
                     <v-card-text class="white--text">
-                      <h3 class="text-center">Don't Have an Account Yet?</h3>
-                      <h6 class="text-center">
-                        Let's get you all set up so you can start creating your
-                        your first<br />
-                        onboarding experience
-                      </h6>
+                      <h3 class="text-center">¿No tienes una cuenta?</h3>
                     </v-card-text>
                     <div class="text-center">
-                      <v-btn tile outlined dark @click="step++">SIGN UP</v-btn>
+                      <v-btn tile outlined dark @click="step++"
+                        >REGISTRATE</v-btn
+                      >
                     </div>
                   </div>
                 </v-col>
@@ -92,104 +78,116 @@
                 <v-col cols="12" md="6" class="blue rounded-br-xl">
                   <div style="text-align: center; padding: 180px 0">
                     <v-card-text class="white--text">
-                      <h3 class="text-center">Alredy Signed up?</h3>
-                      <h6 class="text-center">
-                        Log in to your account so you can continue building
-                        and<br />
-                        editing your onboarding flows
-                      </h6>
+                      <h3 class="text-center">¿Ya estás registrado?</h3>
+                      <h6 class="text-center">Inicia sesión en tu cuenta</h6>
                     </v-card-text>
                     <div class="text-center">
-                      <v-btn tile outlined dark @click="step--">Log in</v-btn>
+                      <v-btn tile outlined dark @click="step--"
+                        >INICIAR SESIÓN</v-btn
+                      >
                     </div>
                   </div>
                 </v-col>
 
                 <v-col cols="12" md="6">
-                  <v-card-text class="mt-12">
-                    <h4 class="text-center">Sign Up for an Account</h4>
-                    <h6 class="text-center grey--text">
-                      Let's get you all set up so you can start creatin your
-                      <br />
-                      first onboarding experiance
-                    </h6>
-                    <v-row align="center" justify="center">
-                      <v-col cols="12" sm="8">
-                        <v-row>
-                          <v-col cols="12" sm="6">
-                            <v-text-field
-                              label="First Name"
-                              outlined
-                              dense
-                              color="blue"
-                              autocomplete="false"
-                              class="mt-4"
-                            />
-                          </v-col>
-                          <v-col cols="12" sm="6">
-                            <v-text-field
-                              label="Last Name"
-                              outlined
-                              dense
-                              color="blue"
-                              autocomplete="false"
-                              class="mt-4"
-                            />
-                          </v-col>
-                        </v-row>
-                        <v-text-field
-                          label="Email"
-                          outlined
-                          dense
-                          color="blue"
-                          autocomplete="false"
-                        />
-                        <v-text-field
-                          label="Password"
-                          outlined
-                          dense
-                          color="blue"
-                          autocomplete="false"
-                          type="password"
-                        />
-                        <v-row>
-                          <v-col cols="12" sm="7">
-                            <v-checkbox
-                              label="I Accept AAE"
-                              class="mt-n1"
-                              color="blue"
-                            >
-                            </v-checkbox>
-                          </v-col>
-                          <v-col cols="12" sm="5">
-                            <span class="caption blue--text ml-n4"
-                              >Terms &Conditions</span
-                            >
-                          </v-col>
-                        </v-row>
-                        <v-btn color="blue" dark block tile>Sign up</v-btn>
+                  <v-form ref="form" v-model="valid" miValidacion>
+                    <v-card-text class="mt-12">
+                      <h4 class="text-center">
+                        Registrate para crear tu cuenta
+                      </h4>
 
-                        <h5 class="text-center grey--text mt-4 mb-3">
-                          Or Sign up using
-                        </h5>
-                        <div
-                          class="d-flex justify-space-between align-center mx-10 mb-11"
-                        >
-                          <v-btn depressed outlined color="grey">
-                            <v-icon color="red">fab fa-google</v-icon>
-                          </v-btn>
-                          <v-btn depressed outlined color="grey">
-                            <v-icon color="blue">fab fa-facebook-f</v-icon>
-                          </v-btn>
-                          <v-btn depressed outlined color="grey">
-                            <v-icon color="light-blue lighten-3"
-                              >fab fa-twitter</v-icon
-                            >
-                          </v-btn>
-                        </div>
-                      </v-col>
-                    </v-row>
-                  </v-card-text>
+                      <v-row align="center" justify="center">
+                        <v-col cols="12" sm="8">
+                          <v-row>
+                            <v-col cols="12" sm="6">
+                              <v-text-field
+                                v-model="registerNombre"
+                                label="Nombre"
+                                outlined
+                                dense
+                                color="blue"
+                                autocomplete="false"
+                                class="mt-4"
+                                :rules="validacionNombre"
+                                required
+                              />
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                              <v-text-field
+                                v-model="registerApellidos"
+                                label="Apellidos"
+                                outlined
+                                dense
+                                color="blue"
+                                autocomplete="false"
+                                class="mt-4"
+                                :rules="validacionNombre"
+                                required
+                              />
+                            </v-col>
+                          </v-row>
+                          <v-text-field
+                            v-model="registerMail"
+                            label="Correo Electrónico"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                            :rules="validacionMail"
+                            required
+                          />
+                          <v-text-field
+                            v-model="registerPwd"
+                            label="Contraseña"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                            type="password"
+                            :rules="validacionContraseña"
+                            required
+                          />
+                          <v-text-field
+                            v-model="registerConfirmPwd"
+                            label="Repetir Contraseña"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                            type="password"
+                            :rules="
+                              validacionContraseña.concat(
+                                passwordConfirmationRule
+                              )
+                            "
+                            required
+                          />
+                          <v-row>
+                            <v-col cols="12" sm="9">
+                              <v-checkbox
+                                v-model="registerChecked"
+                                :rules="validacionCheck"
+                                label="Acepto términos y condiciones"
+                                class="mt-n1"
+                                color="blue"
+                                required
+                              >
+                              </v-checkbox>
+                            </v-col>
+                          </v-row>
+                          <v-btn
+                            :disabled="!valid"
+                            color="blue"
+                            @click="register()"
+                            dark
+                            block
+                            tile
+                            >REGISTRARME</v-btn
+                          >
+                        </v-col>
+                      </v-row>
+                    </v-card-text>
+                  </v-form>
                 </v-col>
               </v-row>
             </v-window-item>
@@ -202,11 +200,70 @@
 
 <script>
 export default {
+  /*eslint-disable */
+
+  /*Documentacion de Vuetify Validacion */
+  /*https://vuetifyjs.com/en/components/forms/#vuelidate */
   data: () => ({
+    valid: true,
+    emailLogIn: "",
+    pwdLogIn: "",
+    registerNombre: "",
+    registerApellidos: "",
+    registerMail: "",
+    registerPwd: "",
+    registerConfirmPwd: "",
+    registerChecked: false,
     step: 1,
+    validacionNombre: [
+      (val) => !!val || "El nombre es obligatorio",
+      (val) =>
+        (val && val.length <= 15) || "El nombre debe ser menor a 10 caracteres",
+    ],
+    validacionContraseña: [
+      (val) =>
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/.test(val) ||
+        " 8 a 24 caracteres Debe incluir Letras maysuculas y minusculas, un numero y un caracter especial",
+    ],
+    validacionMail: [
+      (val) =>
+        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(val) ||
+        "Debe contener un @, una extensión . al final. Ejemplo: micorreo@gmail.com",
+    ],
+    validacionRepetirContra: [
+      (val) =>
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/.test(val) ||
+        " 8 a 24 caracteres Debe incluir Letras maysuculas y minusculas, un numero y un caracter especial",
+    ],
+    validacionCheck: [
+      (v) => !!v || "Debes aceptar terminos y condiciones para registrarte",
+    ],
   }),
+  methods: {
+    logIn() {
+      console.log("Hola");
+      this.$refs.form.validate();
+    },
+    register() {
+      console.log("Hola");
+      this.$refs.form.validate();
+    },
+  },
   props: {
     source: String,
+  },
+
+  /*Para validar contraseñas tenemos que llamar a varias variables de data, como es un metodo mas complego, 
+  realizamos un metodo para validar en computed
+  llamando variables de data, luiego en template llamamos al metodo en :rules=metodo1.concat(metodo2) 
+  para aplicar las reglas de distintos metodos, pues son dos arrays con metodos y buscamos concatenarlos
+  */
+  computed: {
+    passwordConfirmationRule() {
+      return () =>
+        this.registerPwd === this.registerConfirmPwd ||
+        "Las contraseñas deben coincidir";
+    },
   },
 };
 </script>
