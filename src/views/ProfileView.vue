63cfaafc1b5bc9f12952987c9b7fd893fb151c54
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-02-18 14:22:12 +0100

    trabajdnmo en perfl de usuia

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -2,31 +2,57 @@
   <NavBar></NavBar>
 
   <div class="contenedor">
-    <v-card class="elevation-6 mt-10 formCard">
-      <h1 class="text-center">{{ userData.userName }}</h1>
+    <v-card class="elevation-6 mt-10 formCard" v-if="cargando">
+      <h1 class="text-center">
+        {{ userComprobar.nombre + " " + userComprobar.lastName }}
+      </h1>
       <v-avatar class="avatar" color="surface-variant" size="200"></v-avatar>
+      <v-btn
+        v-if="!editar"
+        color="success"
+        style="display: flex; margin: 0 auto; margin-bottom: 2rem"
+        @click="this.editar = true"
+        ><v-icon style="padding-right: 0.5rem">mdi-pencil </v-icon>EDITAR
+        INFORMACIÓN</v-btn
+      >
+      <v-btn
+        v-else
+        color="error"
+        style="display: flex; margin: 0 auto; margin-bottom: 2rem"
+        @click="this.editar = false"
+      >
+        <v-icon style="padding-right: 0.5rem">mdi-cancel </v-icon> CANCELAR
+      </v-btn>
+
       <v-row align="center" justify="center">
         <v-col cols="12" sm="8">
-          <v-form class="profileForm" ref="form" miValidacion>
+          <v-form
+            class="profileForm"
+            ref="form"
+            miValidacion
+            v-model="valid"
+            :disabled="!editar"
+          >
             <v-row>
               <v-col cols="12" sm="6">
                 <v-text-field
-                  v-model="userData.userName"
+                  v-model="userData.nombre"
                   label="Nombre"
                   variant="solo"
                   clearable
+                  active
                   color="blue"
                   class="mt-4"
                   :rules="this.validacionNombre"
-                  required
                 />
               </v-col>
               <v-col cols="12" sm="6">
                 <v-text-field
-                  v-model="userName"
+                  v-model="userData.lastName"
                   label="Apellidos"
                   outlined
                   dense
+                  active
                   color="blue"
                   autocomplete="false"
                   class="mt-4"
@@ -36,19 +62,21 @@
               </v-col>
             </v-row>
             <v-text-field
-              v-model="userData.userMail"
+              v-model="userData.mail"
               label="Correo Electrónico"
               outlined
               dense
+              active
               color="blue"
               autocomplete="false"
               :rules="validacionMail"
               required
             />
             <v-text-field
-              v-model="userData.userPwd"
+              v-model="userData.pwd"
               label="Contraseña"
               outlined
+              active
               dense
               color="blue"
               autocomplete="false"
@@ -56,10 +84,12 @@
               :rules="validacionContraseña"
             />
             <v-text-field
+              v-if="editar"
               v-model="registerConfirmPwd"
               label="Repetir Contraseña"
               outlined
               dense
+              active
               color="blue"
               autocomplete="false"
               type="password"
@@ -80,6 +110,7 @@
               </v-col>
             </v-row>
             <v-btn
+              v-if="editar"
               :disabled="!valid"
               color="blue"
               @click="register()"
@@ -92,6 +123,7 @@
         </v-col>
       </v-row>
     </v-card>
+    <h1 v-else>CARGANGOD...</h1>
   </div>
 </template>
 
@@ -101,18 +133,23 @@ import { API_URL } from "@/helpers/basicFunctions";
 /*eslint-disable */
 export default {
   async created() {
+    this.cargando = false;
     debugger;
+    this.userComprobar = await this.fetchUserData(
+      this.$store.state.currentUser
+    );
     this.userData = await this.fetchUserData(this.$store.state.currentUser);
+    debugger;
     console.log(this.userData);
+    this.cargando = true;
   },
   data() {
     return {
-      userData: {
-        userName: "Rafael",
-        userLastName: "Palomino Rodríguez",
-        userMail: "micorreo@gmail.com",
-        userPwd: "Rafapr_01#",
-      },
+      valid: false,
+      editar: false,
+      cargando: false,
+      userData: null,
+      userComprobar: null,
       test: "true",
       esValido: false,
       valid: true,
@@ -128,7 +165,7 @@ export default {
       ],
       validacionContraseña: [
         (val) =>
-          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/.test(val) ||
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,24}$/.test(val) ||
           " 8 a 24 caracteres Debe incluir Letras maysuculas y minusculas, un numero y un caracter especial",
       ],
       validacionMail: [
@@ -138,7 +175,7 @@ export default {
       ],
       validacionRepetirContra: [
         (val) =>
-          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/.test(val) ||
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,24}$/.test(val) ||
           " 8 a 24 caracteres Debe incluir Letras maysuculas y minusculas, un numero y un caracter especial",
       ],
       validacionCheck: [
@@ -162,21 +199,23 @@ export default {
   computed: {
     passwordConfirmationRule() {
       return () =>
-        this.userData.userPwd === this.registerConfirmPwd ||
+        this.userData.pwd === this.registerConfirmPwd ||
         "Las contraseñas deben coincidir";
     },
     validateName() {
-      console.log(this.userData.userName);
-      this.userData.userName.length <= 15 ? true : false;
+      console.log(this.userData.nombre);
+      this.userData.nombre.length <= 15 ? true : false;
     },
   },
   watch: {
+    /*
     userData: {
       handler(newVal, oldVal) {
-        this.validateAll();
+        if()
       },
       deep: true,
     },
+    */
   },
   components: { NavBar },
 };
